41b17c4f5f5135ce4c6bf9bfc39e9899aebd7045
himanish <himanish.kaushal@gmail.com>
2022-01-05 07:42:16 -0500

    get and set functional for case: cache is empty

--- lru_cache.rb
+++ lru_cache.rb
@@ -1,14 +1,45 @@
 class LruCache
 
+    MAX = 5
+
+    def initialize
+        @lru
+        @mru
+        @cache = {}
+    end
+
     def get(key)
-        puts 'cache is empty'
+        puts 'cache is empty' if @cache.size == 0
     end
 
     def set(key, value)
-        puts "added value '#{value}' for key #{key}"
+        new_node = Node.new(value)
+        if (@cache.size == 0)
+            @cache[key] = new_node
+            @lru = key
+            @mru = key
+        end
+
+        print_cache_state
+    end
+
+    private
+
+    def print_cache_state
+        puts "cache state: #{@cache}"
+        puts "lru: #{@lru}"
+        puts "mru: #{@mru}"
+    end
+
+    class Node
+        attr_accessor :value, :next
+
+        def initialize(value)
+            @value = value
+        end
     end
 end
 
 cache = LruCache.new
 cache.get 'first'
-cache.set 'first', 'First'
+cache.set('first', 'himanish')
